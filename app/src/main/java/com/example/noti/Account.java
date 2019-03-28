e87a7cda7ca09f24a10574c87b10fd7f068ce2b0
nunmino <nun555nun@gmail.com>
2019-03-28 16:25:54 +0700

    ud

--- app/src/main/java/com/example/noti/Account.java
+++ app/src/main/java/com/example/noti/Account.java
@@ -1,7 +1,9 @@
 package com.example.noti;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +12,9 @@ import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.util.Patterns;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
@@ -21,6 +25,8 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -41,10 +47,15 @@ public class Account extends AppCompatActivity {
     private FirebaseDatabase database;
     public DatabaseReference dbRef;
     public static final String NODE_fcm = "fcm-token";
+    public ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accountt);
+        progressDialog = new ProgressDialog(Account.this);
+        progressDialog.setMessage("Loading.....");
+        progressDialog.setTitle("กำลังโหลดข้อมูล");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -84,12 +95,11 @@ public class Account extends AppCompatActivity {
         tv_pass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(tv_pass.getText().equals("1")){
+                if (tv_pass.getText().equals("1")) {
                     tv_pass.setText("0");
                     passwordEditText.setTransformationMethod(null);
                     tv_pass.setBackgroundResource(R.drawable.ic_visibility_black_24dp);
-                }
-                else{
+                } else {
                     tv_pass.setText("1");
                     passwordEditText.setTransformationMethod(new PasswordTransformationMethod());
                     tv_pass.setBackgroundResource(R.drawable.ic_visibility_off_black_24dp);
@@ -98,13 +108,92 @@ public class Account extends AppCompatActivity {
         });
 
 
-
         Button editPassword = findViewById(R.id.edit_pass_button);
         editPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (passwordEditText.length() >= 6) {
-                    changePassword();
+
+                    AlertDialog.Builder builder =
+                            new AlertDialog.Builder(Account.this);
+                    LayoutInflater inflater = getLayoutInflater();
+
+                    View view = inflater.inflate(R.layout.activity_custom_dialog_login, null);
+                    builder.setView(view);
+
+                    final EditText username = (EditText) view.findViewById(R.id.username);
+                    final EditText password = (EditText) view.findViewById(R.id.password);
+
+                    final TextView tv_visible = view.findViewById(R.id.pass_visible);
+                    password.addTextChangedListener(new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+                            if (password.getText().length() > 0) {
+                                tv_visible.setVisibility(View.VISIBLE);
+                            } else {
+                                tv_visible.setVisibility(View.GONE);
+                            }
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable s) {
+
+                        }
+                    });
+
+                    tv_visible.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (tv_visible.getText().equals("1")) {
+                                tv_visible.setText("0");
+                                password.setTransformationMethod(null);
+                                tv_visible.setBackgroundResource(R.drawable.ic_visibility_black_24dp);
+                            } else {
+                                tv_visible.setText("1");
+                                password.setTransformationMethod(new PasswordTransformationMethod());
+                                tv_visible.setBackgroundResource(R.drawable.ic_visibility_off_black_24dp);
+                            }
+                        }
+                    });
+
+                    builder.setPositiveButton("Login", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Check username password
+                            AuthCredential credential = EmailAuthProvider
+                                    .getCredential(username.getText().toString(), password.getText().toString());
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            user.reauthenticate(credential)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            Log.d("test", "User re-authenticated.");
+                                            progressDialog.show();
+                                            if (task.isSuccessful()) {
+                                                changePassword();
+                                            } else {
+                                                progressDialog.dismiss();
+                                                Toast.makeText(Account.this, "อีเมล์หรือรหัสผ่านไม่ถูกต้อง", Toast.LENGTH_SHORT).show();
+
+                                            }
+                                        }
+                                    });
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            passwordEditText.setText("");
+                        }
+                    });
+
+                    builder.show();
+
                 } else {
                     Toast.makeText(Account.this, "กรุณากรอกรหัสผ่าน 6 ตัวอักษรขึ้นไป", Toast.LENGTH_SHORT).show();
                 }
@@ -128,14 +217,13 @@ public class Account extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if(!Patterns.EMAIL_ADDRESS.matcher(emailEditText.getText().toString()).matches()||emailEditText.getText().toString().length()==0){
+                if (!Patterns.EMAIL_ADDRESS.matcher(emailEditText.getText().toString()).matches() || emailEditText.getText().toString().length() == 0) {
                     emailEditText.setError("โปรดระบุ email ให้ถูกต้อง");
                     emailEditText.requestFocus();
-                }
-                else{
+                } else {
                     new AlertDialog.Builder(Account.this)
                             .setTitle("แก้ไข email")
-                            .setMessage("ต้องการเปลี่ยน email เป็น "+emailEditText.getText().toString()+" ใช่หรือไม่")
+                            .setMessage("ต้องการเปลี่ยน email เป็น " + emailEditText.getText().toString() + " ใช่หรือไม่")
                             .setPositiveButton("ใช่", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
@@ -279,6 +367,7 @@ public class Account extends AppCompatActivity {
                             } else {
                                 Toast.makeText(Account.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
                             }
+                            progressDialog.dismiss();
                         }
                     });
         }
---
+++ app/src/main/java/com/example/noti/CustomDialogLogin.java
@@ -0,0 +1,13 @@
+package com.example.noti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomDialogLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_dialog_login);
+    }
+}
